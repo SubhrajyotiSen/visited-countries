e2d6fdb6a0cdd44c1cd55ecf1bf81ddbb2951212
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2026-02-14 20:01:41 +0100

    Update countries

--- countries.js
+++ countries.js
@@ -1,43 +1,10 @@
 countries = [
-  "BR", // Brazil
-  "EC", // Ecuador
-  "CL", // Chile
-  "AR", // Argentina
-  "UY", // Uruguay
-  "PY", // Paraguay
-  "PE", // Peru
-  "BO", // Bolivia
-  "CO", // Colombia
   "US", // USA
-  "IE", // Ireland
   "GB", // United Kingdom
-  "FR", // France
-  "IT", // Italy
   "NL", // Netherlands
   "CZ", // Czech Republic
-  "BE", // Belgium
-  "ES", // Spain
-  "DE", // Germany
-  "HT", // Haiti
-  "ZA", // South Africa
-  "UG", // Uganda
-  "MZ", // Mozambique
-  "NG", // Nigeria
   "IN", // India
   "TH", // Thailand
-  "ID", // Indonesia
-  "TR", // Turkey
-  "HR", // Croatia
-  "HU", // Hungary
-  "PT", // Portugal
   "AT", // Austria
-  "MC", // Monaco
-  "CH", // Switzerland
-  "ME", // Montenegro
-  "AL", // Albania
-  "BA", // Bosnia
-  "GR", // Greece
-  "SG", // Singapore
   "VN", // Vietnam
-  "PH", // Philippines
 ];
